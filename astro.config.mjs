cff49f2a95e41bab5108d5f5d31f3ecb127ea97e
weilin lai <boblai719@gmail.com>
2026-07-09 16:38:20 +0800

    vessel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind"; 
+import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://weilinlai719.github.io',
@@ -7,6 +8,7 @@ export default defineConfig({
   base: '/personal',
   integrations: [tailwind()],
 output: 'server',
+adapter: vercel(),
   build: {
     format: 'directory'
   }
