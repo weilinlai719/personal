500f60e47387fdeb193261eaa88b5f8d22d0a15c
weilin lai <boblai719@gmail.com>
2026-08-24 16:59:03 +0800

    w

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind"; 
-import vercel from '@astrojs/vercel'; 
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'server', 
-  adapter: vercel()
+  output: 'server',
+  adapter: vercel(),
+  integrations: [
+    tailwind({
+      // 💡 顯式指定 Tailwind 搜尋 HTML/Astro 檔案的路徑，消除 Warning
+      config: {
+        content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+      },
+    }),
+  ],
 });
