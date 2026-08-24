4482eec1fc41c306d9fa6fc6e38482ce82ed78fb
weilin lai <boblai719@gmail.com>
2026-08-24 19:44:23 +0800

    ew

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
+
   vite: {
-    plugins: [tailwindcss()],
-  },
+    plugins: [tailwindcss()]
+  }
 });
